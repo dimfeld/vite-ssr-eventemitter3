aaccb89f592c4acc45a6c531e9eae31518e83529
Daniel Imfeld <daniel@imfeld.dev>
2021-10-30 14:33:52 -1000

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { WalletError } from '@solana/wallet-adapter-base';
+
+  let x = new WalletError();
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{x}
